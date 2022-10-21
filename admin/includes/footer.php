25eb2bbae6bbdde8d47e9999f369075ae929a4e4
code-bajju <11212766@mmec.mmdu.org>
2022-10-21 04:59:25 -0400

    lms

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2022 Online Library Management System |<a href="https://www.linkedin.com/in/bajrang-gour/" target="_blank"  > Designed by : Bajrang Gour </a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
